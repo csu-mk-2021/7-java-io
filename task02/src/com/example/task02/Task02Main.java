259e05b6f57751035434598a340ae87bda8d9adb
agridnev228 <agridnev228@gmail.com>
2021-10-27 20:47:39 +0500

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -9,5 +9,23 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        strToUnix();
+    }
+
+    public static void strToUnix() throws IOException {
+        int cur = System.in.read();
+        while (cur != -1) {
+            int next = System.in.read();
+            if (next == -1) {
+                System.out.write(cur);
+            } else if (cur == 13 && next == 10) {
+                System.out.write(next);
+                next = System.in.read();
+            } else {
+                System.out.write(cur);
+            }
+            cur = next;
+        }
+        System.out.flush();
     }
 }
