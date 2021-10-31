1d2caa4a4f52ada0805b1a69f0f608d7739698c9
Sovest <fedorovama@gmail.com>
2021-10-31 16:23:49 +0300

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -9,5 +9,33 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        WinStrToUnixStr();
+    }
+
+    public static void WinStrToUnixStr() throws IOException {
+
+
+        int current_symb = System.in.read();
+
+
+        while (current_symb != -1) {
+
+            int next_symb = System.in.read();
+
+            if (next_symb == -1) {
+                System.out.write(current_symb);
+            } else if (current_symb == 13 && next_symb == 10) {
+                System.out.write(next_symb);
+                next_symb = System.in.read();
+            } else {
+                System.out.write(current_symb);
+            }
+
+            current_symb = next_symb;
+
+        }
+
+        System.out.flush();
+
     }
 }
