51b6ddf73cc0a58ab8cb42407207e6dbe681c2fd
blackrose198 <grushinskayan@gmail.com>
2021-10-31 23:02:22 +0500

    task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -4,18 +4,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 
+import java.io.*;//для работы со всем вводом/выводом
+
 public class Task03Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        if (inputStream == null || charset == null){
+            throw new IllegalArgumentException("some of argument is NULL");
+        }
+        ByteArrayOutputStream bytes_mass = new ByteArrayOutputStream(); //поток байтов, записываемые в буфер для чтения и записи
+        byte[] bytes = new byte[500];
+        int count;
+        while ((count = inputStream.read(bytes)) != -1) {
+            bytes_mass.write(bytes, 0, count);//(byte[] array, int start, int limit)
+        }
+        return new String(bytes_mass.toByteArray(), charset); //charset - выбор кодировки
+
     }
 }
