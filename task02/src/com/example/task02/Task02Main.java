a1fdf071f946a2b965859566a60f6e229aafca78
danil <danilmurmya@gmail.com>
2021-11-02 12:09:31 +0500

    task02 done.

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -4,10 +4,26 @@ import java.io.IOException;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        WindowsStringToUnix();
+    }
+    public static void WindowsStringToUnix() throws IOException {
+        int current_buffer = System.in.read();
+        int next = 0;
+
+
+        while (current_buffer != -1){
+            next = System.in.read();
+            if(next == -1){
+                System.out.write(current_buffer);
+            }
+            else if (current_buffer == 13 && next == 10){
+                System.out.write(next);
+                next = System.in.read();
+            } else {
+                System.out.write(current_buffer);
+            }
+            current_buffer = next;
+        }
+        System.out.flush();
     }
 }
