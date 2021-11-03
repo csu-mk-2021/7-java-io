b52ba33add4a6af02f9c7180829902ee4dd51712
AntyufeevA <awsd.artem.2k00@gmail.com>
2021-11-03 22:01:07 +0500

    task03 []

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,6 +3,7 @@ package com.example.task03;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.io.*;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
@@ -15,7 +16,17 @@ public class Task03Main {
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        ByteArrayOutputStream byte_array = new ByteArrayOutputStream();
+        byte[] bytes = new byte[500];
+        int count = 0;
+
+        if (inputStream == null || charset == null){
+            throw new IllegalArgumentException("Null argument value");
+        }
+
+        while ((count = inputStream.read(bytes)) != -1) {
+            byte_array.write(bytes, 0, count);
+        }
+        return new String(byte_array.toByteArray(), charset);
     }
 }
