6002a1979b4a7ffe40fecf5da6f915dfb1471981
MagTemTenebrius <magtemtenebrius@gmail.com>
2021-12-20 11:11:38 +0500

    task02 successful

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,7 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +10,15 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        InputStream is = System.in;
+        int b_number = is.read();
+        int number;
+        while (b_number > 0) {
+            number = is.read();
+            if (b_number != 13 || number != 10)
+                System.out.write(b_number);
+            b_number = number;
+        }
+        System.out.flush();
     }
 }
